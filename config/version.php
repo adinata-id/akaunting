754a4a7651ff95181acc54839fe4ca85699ce995
adinata-id <adinata4646@gmail.com>
2021-11-13 20:37:07 +0700

    first commit

---
+++ config/version.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'name'      =>  'Akaunting',
+
+    'code'      =>  'Document',
+
+    'major'     =>  '2',
+
+    'minor'     =>  '1',
+
+    'patch'     =>  '27',
+
+    'build'     =>  '',
+
+    'status'    =>  'Stable',
+
+    'date'      =>  '08-November-2021',
+
+    'time'      =>  '15:00',
+
+    'zone'      =>  'GMT +3',
+
+];
